9ff7fb69dd42fbce53e8cae84c176408d0d17ff3
lucas13146 <83290439+lucas13146@users.noreply.github.com>
2021-08-22 14:54:25 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,143 +1,97 @@
-#----------------[Assets]----------------#
+#---------[Assets]--------#
 import discord
 from discord.ext import commands
-from discord.ext.commands import has_permissions, MissingRequiredArgument, MissingPermissions
+from dotenv import load_dotenv
 import os
-import sys
-import json
 import asyncio
 
-prefix = "-"
-token = "ODUwMzkwMDQ0ODMyNDMyMTU5.YLpBcg.n7hGMxXnm-oXaRkkrwThONZk-l8"
+load_dotenv()
 
-client = commands.Bot(command_prefix=prefix, help_command=None)
+owner_id = os.getenv('OWNER_ID')
+prefix = os.getenv('PREFIX')
+token = os.getenv('TOKEN')
 
-#----------------[Ready]----------------#
+intents = discord.Intents(messages=True, guilds=True, members=True)
+intents.members = True
+intents.reactions = True
+intents.messages = True
+intents.emojis = True
+intents.all()
 
-temp_channel_id = None
+client = commands.Bot(command_prefix=prefix, help_command=None, intents=intents)
 
-async def get_temp_channel():
-    with open(f"bdd/temp.json", "r") as f:
-        channel = json.load(f)
-
-    return channel
-
-
-async def open_temp_channel(channel):
-    data = await get_temp_channel()
-
-    data["channel"] = int(channel.id)
-
-    with open(f"bdd/temp.json", "w") as f:
-        json.dump(data, f)
-
-    return True
+#---------[Ready]--------#
 
 @client.event
 async def on_ready():
-    print("Bot connected ...")
-    data = await get_temp_channel()
-    if data["channel"] != 0:
-        channel_id = data["channel"]
-        chnl = client.get_channel(channel_id)
-        await chnl.send("Bot online ...")
-        data["channel"] = 0
-        with open(f"bdd/temp.json", "w") as f:
-            json.dump(data, f)
+    os.system('cls')
+    print("Le bot est on ...")
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name=f"{prefix}help"))
 
-#----------------[Owner]----------------#
+#---------[Ping]--------#
 
-async def get_permision_members():
-    with open(f"bdd/perm.json", "r") as f:
-        channel = json.load(f)
+@client.command(name="ping")
+async def ping(ctx):
+    if(ctx):
+        await ctx.message.delete()
 
-    return channel
+    em = discord.Embed(
+        title=f"Pong",
+        color=discord.Color.from_rgb(255, 85, 85)
+    )
 
+    await ctx.send(embed=em)
 
-async def open_permision_account(member, perm):
-    data = await get_permision_members()
+#---------[reaction-role]--------#
 
-    if str(member.id) in data:
-        return False
-    else:
-        data[str(member.id)] = perm
+msg_reaction = []
 
-    with open(f"bdd/perm.json", "w") as f:
-        json.dump(data, f)
+@client.command(name="reaction-role")
+async def reaction(ctx):
+    if(ctx):
+        await ctx.message.delete()
 
-    return True
+    em = discord.Embed(
+        title=f"Reaction Role :",
+        description=f"Jaune : `🟡` \n\n Rouge : `🔴`",
+        color=discord.Color.from_rgb(255, 85, 85)
+    )
 
-@client.command(name="set_owner")
-async def set_owner(ctx, member:discord.Member = None):
-    guild = ctx.guild
-    if member == None:
-        member = ctx.author
-    else:
-        member = member
-    
-    if member.id == 560849136899850261:
-        await open_permision_account(member, "owner")
+    message = await ctx.send(embed=em)
+    await message.add_reaction("🟡")
+    await message.add_reaction("🔴")
+    msg_reaction.append(message.id)
 
-        msg = await ctx.send(f"Les permision de owner on bien été ajouté à : **{member.name}**")
-        await asyncio.sleep(4)
-        await msg.delete()
-    else:
-        msg = await ctx.send("Tu a pas la permision de fair sa ...")
-        await asyncio.sleep(4)
-        await msg.delete()
+@client.event
+async def on_raw_reaction_add(payload):
+    message_id = payload.message_id
+    channel = client.get_channel(payload.channel_id)
+    message = await channel.fetch_message(message_id)
+    guild_id = payload.guild_id
+    user = payload.member
+    guild = client.get_guild(guild_id)
 
-#----------------[Restart]----------------#
+    if message_id in msg_reaction:
+        if payload.emoji.name == "🟡":
+            if payload.user_id != client.user.id:
+                role_jaune = guild.get_role(878980986875043910)
+                await user.add_roles(role_jaune)
+                await message.remove_reaction("🟡", user)
 
-def restart_program():
-    python = sys.executable
-    os.execl(python, python, * sys.argv)
+        if payload.emoji.name == "🔴":
+            if payload.user_id != client.user.id:
+                role_rouge = guild.get_role(878981055330271252)
+                await user.add_roles(role_rouge)
+                await message.remove_reaction("🔴", user)
 
-@client.command()
-async def restart(ctx):
-    data_perm = await get_permision_members()
-    if (data_perm):
-        if str(ctx.author.id) in data_perm:
-            await ctx.message.delete()
-            message = await ctx.send("Restarting...")
-            temp_channel = ctx.channel
-            await open_temp_channel(temp_channel)
-            restart_program()
-        else:
-            msg = await ctx.send("Tu a pas la permision de fair sa ...")
-            await asyncio.sleep(4)
-            await msg.delete()
-    else:
-        msg = await ctx.send("Tu a pas la permision de fair sa ...")
-        await asyncio.sleep(4)
-        await msg.delete()
+#---------[on_member_join]--------#
 
-#----------------[Clear]----------------#
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(878972029670420542)
+    await channel.send(f"Le joueur `{member.name}` a rejoint le serveur ...")
 
-@client.command(name="clear", pass_content=True)
-@has_permissions(manage_messages=True)
-async def clear(ctx, number):
-    await ctx.channel.purge(limit=int(number) + 1)
-
-
-@clear.error
-async def clear_error(ctx, error):
-    if isinstance(error, MissingPermissions):
-        embed = discord.Embed(
-            title="❌Il y a une erreur :",
-            description="Vous avez pas la permission de fair sa !",
-            color=discord.Color.from_rgb(47, 49, 54)
-        )
-        await ctx.send(embed=embed)
-    if isinstance(error, MissingRequiredArgument):
-        embed = discord.Embed(
-            title="❌ Mauvaise utilisation de la commande.",
-            description=f"`{prefix}clear <nombre>`",
-            color=discord.Color.red()
-        )
-        await ctx.send(embed=embed)
-
-#----------------[Login]----------------#
-
+#---------[Login]--------#
 def run():
     client.run(token)
 
